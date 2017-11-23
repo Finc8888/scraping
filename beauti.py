f2b827a6dc30e45facd205e9307a5be14b53e273
Vladimir Prikhodko <con8@mail.ru>
2017-11-23 23:09:15 +0300

    Добавлена отловка исключений и идет передача через функцию

--- beauti.py
+++ beauti.py
@@ -1,38 +1,32 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from urllib.error import HTTPError
-html = urlopen("http://www.pythonscraping.com/exercises/exercise1.html")
-html2 = urlopen("http://finc8888.pythonanywhere.com")
-html3 = urlopen("https://time100.ru/Moscow")
-bsObj = BeautifulSoup(html.read(),"html.parser")
-bsObj1 = BeautifulSoup(html2.read(),"html.parser")
-bsObj2 = BeautifulSoup(html3.read(),"html.parser")
-# print(bsObj.h1)
-# print(bsObj1.h1)
-str_out_pars = str(bsObj2.findAll("div", {"class":"time","data-tz":"Europe/Moscow"})[0])
-# print(list_pars)
-ugly_result = str_out_pars.split(">")[1]
-result = ugly_result.split("<")[0]
-print(result)
 
-# def getTitle(url):
-# 	try:
-# 		html = urlopen(url)
-# 	except HTTPError as e:
-# 		return None
-# 	try:
-# 		bsObj = BeautifulSoup(html.read(),"html.parser")
-# 		title = bsObj2.findAll("div", {"class":"time","data-tz":"Europe/Moscow"})[0].split(">")
-# 	except AttributeError as e:
-# 		return None
-# 	return title
-# title = getTitle("http://www.pythonscraping.com/exercises/exercise1.html")
-# if title == None:
-# 	print("Title could not be found")
-# else:
-# 	print(title)
-# title = getTitle("https://time100.ru/Moscow")
-# if title == None:
-# 	print("Title could not be found")
-# else:
-# 	print(title)
+html = urlopen("https://time100.ru/Moscow")
+
+def getTitle(url):
+	try:
+		html = urlopen(url)
+	except HTTPError as e:
+		return None
+	try:
+		bsObj = BeautifulSoup(html.read(),"html.parser")
+		str_out_pars = str(bsObj.findAll("div", {"class":"time","data-tz":"Europe/Moscow"})[0])
+		ugly_result = str_out_pars.split(">")[1]
+		result = ugly_result.split("<")[0]
+	except AttributeError as e:
+		return None
+	return result
+
+result = getTitle("https://time100.ru/Moscow")
+if result == None:
+	print("Результат не найден")
+else:
+	print('Точное московское время-->',result)
+
+# html = urlopen("https://time100.ru/Moscow")
+# bsObj = BeautifulSoup(html.read(),"html.parser")
+# str_out_pars = str(bsObj2.findAll("div", {"class":"time","data-tz":"Europe/Moscow"})[0])
+# ugly_result = str_out_pars.split(">")[1]
+# result = ugly_result.split("<")[0]
+# print('Точное московское время-->',result)
