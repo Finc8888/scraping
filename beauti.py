cc9247e53272dbfde2e4617a800f4a2c7709bb5b
Vladimir Prikhodko <con8@mail.ru>
2017-11-23 22:16:17 +0300

    Парсер точного московского времени онлайн

--- beauti.py
+++ beauti.py
@@ -7,23 +7,32 @@ html3 = urlopen("https://time100.ru/Moscow")
 bsObj = BeautifulSoup(html.read(),"html.parser")
 bsObj1 = BeautifulSoup(html2.read(),"html.parser")
 bsObj2 = BeautifulSoup(html3.read(),"html.parser")
-print(bsObj.h1)
-print(bsObj1.h1)
-print(bsObj2.findAll("div", {"data-tz":"Europe/Moscow"}))
+# print(bsObj.h1)
+# print(bsObj1.h1)
+str_out_pars = str(bsObj2.findAll("div", {"class":"time","data-tz":"Europe/Moscow"})[0])
+# print(list_pars)
+ugly_result = str_out_pars.split(">")[1]
+result = ugly_result.split("<")[0]
+print(result)
 
-def getTitle(url):
-	try:
-		html = urlopen(url)
-	except HTTPError as e:
-		return None
-	try:
-		bsObj = BeautifulSoup(html.read(),"html.parser")
-		title = bsObj.body.h1
-	except AttributeError as e:
-		return None
-	return title
-title = getTitle("http://www.pythonscraping.com/exercises/exercise1.html")
-if title == None:
-	print("Title could not be found")
-else:
-	print(title)
+# def getTitle(url):
+# 	try:
+# 		html = urlopen(url)
+# 	except HTTPError as e:
+# 		return None
+# 	try:
+# 		bsObj = BeautifulSoup(html.read(),"html.parser")
+# 		title = bsObj2.findAll("div", {"class":"time","data-tz":"Europe/Moscow"})[0].split(">")
+# 	except AttributeError as e:
+# 		return None
+# 	return title
+# title = getTitle("http://www.pythonscraping.com/exercises/exercise1.html")
+# if title == None:
+# 	print("Title could not be found")
+# else:
+# 	print(title)
+# title = getTitle("https://time100.ru/Moscow")
+# if title == None:
+# 	print("Title could not be found")
+# else:
+# 	print(title)
